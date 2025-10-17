6c9859bfef6a2a245188525706d1dd0f6bc94719
Josue-H <55160786+Josue-H@users.noreply.github.com>
2025-10-17 00:59:46 -0600

    Update wsgi.py

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.development')
 
 application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(['manage.py', 'runserver', os.environ.get('PORT', '8000')])
