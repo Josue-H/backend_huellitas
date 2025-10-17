16350ba41af89aaf6807269d26d5729868194952
Josue-H <55160786+Josue-H@users.noreply.github.com>
2025-10-17 00:47:10 -0600

    Simplify runserver port handling
    Removed manual port configuration for runserver.

--- manage.py
+++ manage.py
@@ -17,12 +17,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     
-    # Asegúrate de que solo modifiques 'runserver' cuando sea necesario
-    if 'runserver' in sys.argv:
-        # Obtén el puerto desde la variable PORT asignada por Render o 8000 como valor predeterminado
-        port = os.getenv('PORT', 8000)
-        # Modifica sys.argv para pasar el puerto correctamente
-        sys.argv.append(f"0.0.0.0:{port}")  # Ahora se pasa correctamente como argumento al comando
+    # No es necesario agregar el puerto manualmente, ya que Render lo maneja automáticamente
     execute_from_command_line(sys.argv)
 
 
